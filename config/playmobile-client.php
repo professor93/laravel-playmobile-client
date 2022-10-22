6ec98294cc8f070b95b8843e67aef249a81831b7
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2022-10-22 14:05:01 +0500

    config name fixed

--- config/playmobile-client.php
+++ config/playmobile-client.php
@@ -4,9 +4,9 @@
 return [
     'token_lifetime' => env('PLAYMOBILE_TOKEN_DURATION', 24 * 3600 * 30),
 
-    'base_url' => env('PLAYMOBILE_URL'),
-    'username' => env('PLAYMOBILE_USERNAME'),
-    'password' => env('PLAYMOBILE_PASSWORD'),
+    'base_url' => env('PLAYMOBILE_URL', 'https://playmobile.uz/api/v1/'),
+    'username' => env('PLAYMOBILE_USERNAME', 'username'),
+    'password' => env('PLAYMOBILE_PASSWORD', 'password'),
     'originator' => env('PLAYMOBILE_ORIGINATOR', 3900),
 
     'proxy_url' => env('PLAYMOBILE_PROXY_URL'),
